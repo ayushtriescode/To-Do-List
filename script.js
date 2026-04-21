611c052efb8330eb0e2ff3f26499f9c1d9d555cd
Ayush <ayushtriescode@gmail.com>
2026-04-21 11:30:24 +0530

    Added html and logic

---
+++ script.js
@@ -0,0 +1,43 @@
+const input = document.getElementById('itemInput');
+const addBtn = document.getElementById('addBtn');
+const list = document.getElementById('myList');
+
+let itemsArray = JSON.parse(localStorage.getItem('myTasks')) || [] ;
+
+renderlist();
+
+addBtn.addEventListener('click', () => {
+    const text = input.value.trim();
+    if(text !== ""){
+        itemsArray.push(text);
+        updateStorage();
+        renderlist();
+        input.value = "";
+    }
+});
+
+function renderlist() {
+    list.innerHTML = "";
+
+    itemsArray.forEach((item, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <span>${item}</span>
+    <i class="fas fa-trash" onclick="deleteItem(${index})"></i>`;
+    list.appendChild(li)       
+    });
+}
+
+function deleteItem(index) {
+    itemsArray.splice(index, 1);
+    updateStorage();
+    renderlist();
+}
+
+function updateStorage() {
+    localStorage.setItem('myTasks', JSON.stringify(itemsArray));
+}
+
+input.addEventListener('keypress', (e) => {
+    if(e.key === "Enter") addBtn.click();
+});
